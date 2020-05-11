5d2ef9c97833641d606701d250b43510ef502ba2
Jeffrey Yang <jycl1234@users.noreply.github.com>
2020-05-11 11:10:20 -0700

    updated results css

---
+++ src/components/MatSelectorVisual.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="container--image">
+    <div class="wrapper--image" v-for="mat in mats" v-bind:key="mat.filename">
+      <img v-bind:src="getUrl(mat)" v-on:click="handleClick(mat)" />
+    </div>
+  </div>
+</template>
+
+<script>
+import { mats } from "../mats";
+
+export default {
+  methods: {
+    getUrl(mat) {
+      return require("@/assets/" + mat.filename + ".png");
+    },
+    handleClick(mat) {
+      console.log(`${mat.startRange}:${mat.endRange}`);
+    }
+  },
+  data() {
+    return {
+      baseUrl: "./assets/",
+      mats
+    };
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.container--image {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: flex-start;
+  align-items: center;
+  .wrapper--image {
+    img {
+      width: 3.5rem;
+      margin: 0.1rem;
+      cursor: pointer;
+    }
+  }
+}
+</style>
