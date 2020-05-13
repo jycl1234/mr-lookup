26f7fe44e005bda2520bafd6afd487602ced6b39
Jeffrey Yang <jycl1234@users.noreply.github.com>
2020-05-13 14:38:25 -0700

    initial MR data population/parsing

--- src/mats.js
+++ src/mats.js
@@ -5,6 +5,30 @@ export const mats = [
     endRange: "Z18",
     rarity: "bronze",
     type: "mat",
-    filename: "A101"
+    filename: "item_gift_301"
+  },
+  {
+    name: "Mirrors Invitation",
+    startRange: "O26",
+    endRange: "Z30",
+    rarity: "bronze",
+    type: "mat",
+    filename: "item_gift_302"
+  },
+  {
+    name: "Bunny Ear Wrapping Paper",
+    startRange: "O38",
+    endRange: "Z42",
+    rarity: "bronze",
+    type: "mat",
+    filename: "item_gift_303"
+  },
+  {
+    name: "Sheep Warhorn",
+    startRange: "O50",
+    endRange: "Z54",
+    rarity: "bronze",
+    type: "mat",
+    filename: "item_gift_304"
   }
 ];
